5df87479d519caaa415eeda7a3cf174f2f5767b4
Rana Ahmed Barghout <35639718+ranaabarghout@users.noreply.github.com>
2022-09-01 10:54:55 -0400

    Update run_model_wsplits.py

--- DeeplearningApproach/Code/model/run_model_wsplits.py
+++ DeeplearningApproach/Code/model/run_model_wsplits.py
@@ -462,6 +462,9 @@ def split_data(data, input_data=['Compounds', 'Adjacencies', 'Proteins', 'Sequen
         test_len = int(len(new_data)*split[2]/100) # This will need to be changed
     
         clustered_df = new_data.merge(cluster_df, how='left', on='Sequences')
+        print('Shape of clustered_df before shuffling before dropnan: ', len(clustered_df))
+        clustered_df.dropna(inplace=True)
+        print('Shape of clustered_df before shuffling after dropnan: ', len(clustered_df))
         groups = [clustered_df for _, df in clustered_df.groupby('Cluster')]
         random.seed(random_state)
         random.shuffle(groups)
