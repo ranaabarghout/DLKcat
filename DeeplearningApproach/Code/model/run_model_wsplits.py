65d52f2ede5ef9abb98cebf39c9869df839f29bb
Rana Ahmed Barghout <35639718+ranaabarghout@users.noreply.github.com>
2022-09-01 10:16:49 -0400

    Update run_model_wsplits.py

--- DeeplearningApproach/Code/model/run_model_wsplits.py
+++ DeeplearningApproach/Code/model/run_model_wsplits.py
@@ -436,6 +436,7 @@ def split_data(data, input_data=['Compounds', 'Adjacencies', 'Proteins', 'Sequen
          
     elif split_type=='type_4':
         new_data = df
+        print('OG DF:', new_data)
         train_len = int(len(new_data)*split[0]/100) # This will need to be changed
         valid_len = int(len(new_data)*split[1]/100) # This will need to be changed
         test_len = int(len(new_data)*split[2]/100) # This will need to be changed
